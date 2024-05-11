5b842f72243eec175f074a773fc14dda5c2ccfaa
Alheta Wahyu M <alheta.matalarens@gmail.com>
2024-05-12 05:29:26 +0700

    fix: Added PieChart in OverviewFragment

--- app/src/main/java/com/dokari4/personalfinance/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/dokari4/personalfinance/ui/overview/OverviewFragment.kt
@@ -8,13 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.dokari4.personalfinance.R
 import com.dokari4.personalfinance.databinding.FragmentOverviewBinding
+import com.dokari4.personalfinance.domain.model.CategoryCountTotal
+import com.github.mikephil.charting.animation.Easing
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.formatter.PercentFormatter
+import com.github.mikephil.charting.utils.ColorTemplate
 import dagger.hilt.android.AndroidEntryPoint
 
-/**
- * A simple [Fragment] subclass.
- * Use the [OverviewFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 @AndroidEntryPoint
 class OverviewFragment : Fragment() {
     private lateinit var binding: FragmentOverviewBinding
@@ -23,13 +26,51 @@ class OverviewFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentOverviewBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getCategoryTotalTransaction.observe(viewLifecycleOwner) {
+            initPieChart(it)
+        }
+    }
+
+    private fun initPieChart(data: List<CategoryCountTotal>) {
+        val pieEntry = data
+            .filter { it.count > 0 }
+            .map {
+                PieEntry(it.count.toFloat(), it.name)
+            }
+
+//        val colors = listOf(
+//            ColorTemplate.colorWithAlpha(R.color.md_theme_primary, 1),
+//            ColorTemplate.colorWithAlpha(R.color.md_theme_secondary, 1),
+//            ColorTemplate.colorWithAlpha(R.color.md_theme_tertiary, 1),
+//            ColorTemplate.colorWithAlpha(R.color.md_theme_error, 1),
+//        )
+
+        val pieDataSet = PieDataSet(pieEntry, "")
+        pieDataSet.sliceSpace = 3f
+        pieDataSet.colors = ColorTemplate.MATERIAL_COLORS.toList()
+
+        val pieData = PieData(pieDataSet)
+        pieData.setValueFormatter(PercentFormatter(binding.pieChart))
+        pieData.setValueTextSize(24f)
+        pieData.setValueTextColor(R.color.md_theme_background)
+
+        binding.pieChart.data = pieData
+
+        with(binding) {
+            pieChart.setUsePercentValues(true)
+            pieChart.animateY(1000, Easing.EaseInOutQuad)
+            pieChart.description.isEnabled = false
+
+            pieChart.invalidate()
+        }
 
     }
 
